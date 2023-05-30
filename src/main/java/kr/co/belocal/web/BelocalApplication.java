15533652ef8245c2da387ee36ed9acd60f91b489
wonbin <durdlerain@gmail.com>
2023-05-30 20:35:43 +0900

    belocal project init

---
+++ src/main/java/kr/co/belocal/web/BelocalApplication.java
@@ -0,0 +1,13 @@
+package kr.co.belocal.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelocalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/belocal/web/BelocalApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.belocal.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
