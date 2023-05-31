47aa0e07422c666e7985f3ee43973e1f88a695ac
wonbin <durdlerain@gmail.com>
2023-05-31 12:49:51 +0900

    feat: HomeController.java 추가

---
+++ src/main/java/kr/co/belocal/web/controller/HomeController.java
@@ -0,0 +1,16 @@
+package kr.co.belocal.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/index")
+    public String index(Model model) {
+        
+        return "index";
+    }
+    
+}
