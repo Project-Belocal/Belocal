1331b22edb03ca4fd07d23ce74ad59379952687f
imjae <imjae950@gmail.com>
2023-06-03 19:40:52 +0900

    chatroom modal 수정 및 js추가

--- src/main/resources/static/js/chat/chatroom.js
+++ src/main/resources/static/js/chat/chatroom.js
@@ -1,11 +1,39 @@
 window.addEventListener("load",function(){
     const section = document.querySelector(".room");
-    const roomMenu = section.querySelector(".room__menu");
-    const roomNav = section.querySelector(".room__nav");
-    const roomundo = section.querySelector(".room__undo");
+    const Menu = section.querySelector(".room__menu");
+    const Nav = section.querySelector(".room__nav");
+    const Undo = section.querySelector(".room__undo");
+    const reportBtn = section.querySelector(".room__report-btn");
+    const modalBg = section.querySelector(".room__modal-bg");
+    const reportModal = section.querySelector(".room__report");
+    const reportClose = section.querySelector(".room__report-close");
 
-    roomMenu.onclick = function(){
-        roomNav.classList.toggle("hidden");
+    Undo.onclick = function(){
+        history.go(-1);
     }
 
+
+    Menu.onclick = function(){
+        modalBg.classList.toggle("hidden");
+        Nav.classList.toggle("hidden");
+
+    }
+
+    reportBtn.onclick = function(){
+        reportModal.classList.toggle("hidden");
+        Nav.classList.toggle("hidden");
+    }
+
+    reportClose.onclick = function(){
+        reportModal.classList.toggle("hidden");
+        modalBg.classList.toggle("hidden");
+    }
+
+    modalBg.onclick = function(e){
+        if (e.target === modalBg) {
+            modalBg.classList.add("hidden")
+            Nav.classList.add("hidden");
+            reportModal.classList.add("hidden");
+        }
+    }
 })
