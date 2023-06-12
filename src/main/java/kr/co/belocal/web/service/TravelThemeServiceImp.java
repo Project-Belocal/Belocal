781c64658d7d6fdb9ac7d9c10552b566f23500e1
woojeonjang <woojeonjang@gmail.com>
2023-06-12 20:07:10 +0900

    취소할 것임

--- src/main/java/kr/co/belocal/web/service/TravelThemeServiceImp.java
+++ src/main/java/kr/co/belocal/web/service/TravelThemeServiceImp.java
@@ -2,11 +2,14 @@ package kr.co.belocal.web.service;
 
 import java.util.List;
 
+import kr.co.belocal.web.entity.Category;
+import kr.co.belocal.web.entity.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.belocal.web.entity.TravelTheme;
 import kr.co.belocal.web.repository.TravelThemeRepository;
+import org.springframework.ui.context.Theme;
 
 @Service
 public class TravelThemeServiceImp implements TravelThemeService {
@@ -15,10 +18,29 @@ public class TravelThemeServiceImp implements TravelThemeService {
     private TravelThemeRepository repository;
 
     @Override
-    public List<TravelTheme> getList() {
-        List<TravelTheme> list = repository.findAll();
-
-        return list;
+    public List<Theme> getList(Integer memberId) {
+        return null;
     }
-    
+
+    @Override
+    public List<Theme> getList(String categoryName, String location, String description) {
+        return null;
+    }
+
+    @Override
+    public Category findById(Integer memeberId, Integer traverThemeId) {
+        return null;
+    }
+
+    @Override
+    public Member getById(Integer memberId, Integer travelThemeId) {
+        return null;
+    }
+
+    @Override
+    public List<TravelTheme> getList() {
+        return null;
+    }
+
+
 }
