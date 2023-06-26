75fad5aba5c8d3be38656c6a273a7968c7ac14c4
imjae <imjae950@gmail.com>
2023-06-26 19:33:18 +0900

    유저 프로필 파일 업로드 작업 진행중

--- src/main/java/kr/co/belocal/web/entity/Appointment.java
+++ src/main/java/kr/co/belocal/web/entity/Appointment.java
@@ -1,14 +1,8 @@
 package kr.co.belocal.web.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+
+
 public class Appointment {
     private Integer id;
     private Integer travelThemeId;
